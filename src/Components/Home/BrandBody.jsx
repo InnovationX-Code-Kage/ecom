9f3b5a551fc82f02e00cc3b73b604428e12a0e60
Shamya <shamyaharia@gmail.com>
2024-03-19 04:23:39 +0530

    Brand Logos

--- src/Components/Home/BrandBody.jsx
+++ src/Components/Home/BrandBody.jsx
@@ -1,21 +1,21 @@
 import React from 'react'
-import lg from './brand/lg.png';
-import tesla from './brand/tesla.png';
-import bosch from './brand/bosch.png';
-import philips from './brand/philips.png';
-import vguard from './brand/vguard.png';
-import lowes from './brand/lowes.png';
+import oreo from './brand/oreo.png';
+import cadbury from './brand/cadbury.png';
+import nestle from './brand/nestle.png';
+import parle from './brand/parle.webp';
+import kelloggs from './brand/kelloggs.png';
+import sb from './brand/sb.png';
 
 const BrandBody = () => {
   return (
     <div className='brandbody'>
         <div className='brands'>
-            <img src={lg} alt="" width='60px' height='60px' />
-            <img src={tesla} alt="" width='100px' height='80px' />
-            <img src={bosch} alt="" width='200px' height='100px' />
-            <img src={philips} alt="" width='120px' height='70px' />
-            <img src={vguard} alt="" width='150px' height='80px' />
-            <img src={lowes} alt="" width='150px' height='150px' />
+            <img src={cadbury} alt="" width='100px' height='80px' />
+            <img src={nestle} alt="" width='200px' height='100px' />
+            <img src={parle} alt="" width='120px' height='70px' />
+            <img src={oreo} alt="" width='60px' height='60px' />
+            <img src={kelloggs} alt="" width='150px' height='80px' />
+            <img src={sb} alt="" width='150px' height='150px' />
         </div>
         <br/>
         <br/>
